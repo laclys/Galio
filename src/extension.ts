c6be7397cd1c198633b391ba17e2ee049cc28439
Lac <laclys@126.com>
2020-06-23 15:51:13 +0800

    init 0.0.2

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,88 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const GET_OFF_MSG = "已经下班咯, Giao起来！";
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "giao-giao-everyday" is now active!');
+function getTimesMsg(): string {
+  const now = new Date();
+  const toHomeTimes = new Date();
+  toHomeTimes.setHours(
+    vscode.workspace.getConfiguration().get("giao-giao-everyday.toHomeHours") ||
+      0
+  );
+  toHomeTimes.setMinutes(
+    vscode.workspace
+      .getConfiguration()
+      .get("giao-giao-everyday.toHomeMinutes") || 0
+  );
+  toHomeTimes.setSeconds(0);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('giao-giao-everyday.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+  const duration = toHomeTimes.getTime() - now.getTime();
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from giao-giao-everyday!');
-	});
+  if (duration <= 0) {
+    return GET_OFF_MSG;
+  }
+  const hour = Math.floor(duration / 1000 / 60 / 60);
+  const minute = Math.floor((duration / 1000 / 60) % 60);
+  const second = Math.floor((duration / 1000) % 60);
 
-	context.subscriptions.push(disposable);
+  return `>> 距下班还有${hour ? hour + "时" : ""}${
+    minute ? minute + "分" : ""
+  }${second ? second + "秒" : ""}`;
 }
 
-// this method is called when your extension is deactivated
+// 插件被激活时触发，代码总入口；param：context 插件上下文
+export function activate(context: vscode.ExtensionContext) {
+  console.log(
+    'Congratulations, your extension "giao-giao-everyday" is now active!'
+  );
+
+  let giaoFunc = vscode.commands.registerCommand(
+    "giao-giao-everyday.sayGiao",
+    () => {
+      let str = "一给我哩giaogiao！";
+      const name = vscode.workspace
+        .getConfiguration()
+        .get("giao-giao-everyday.name");
+
+      if (name) {
+        str = str + name + ",开心工作，做个乖宝宝";
+      }
+      vscode.window.showInformationMessage(str);
+      // vscode.window.setStatusBarMessage("Giao一下~");
+    }
+  );
+
+  let toHome = vscode.commands.registerCommand(
+    "giao-giao-everyday.toHome",
+    () => {
+      let isGetOff = false;
+
+      vscode.window.setStatusBarMessage(getTimesMsg());
+      const timer = setInterval(() => {
+        let msg = getTimesMsg();
+        vscode.window.setStatusBarMessage(msg);
+        if (msg === GET_OFF_MSG) {
+          if (!isGetOff) {
+            vscode.window.showInformationMessage(msg);
+            isGetOff = true;
+            clearInterval(timer);
+          }
+        } else {
+          isGetOff = false;
+        }
+      }, 1000);
+    }
+  );
+
+  // // 获取所有命令
+  // vscode.commands.getCommands().then((allCommands) => {
+  //   console.log("所有命令：", allCommands);
+  // });
+
+  // 注册命令
+  context.subscriptions.push(giaoFunc);
+  context.subscriptions.push(toHome);
+}
+
+// 插件被释放时触发
 export function deactivate() {}
